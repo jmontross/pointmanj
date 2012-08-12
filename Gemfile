2b454c116399565281b96f96707ae1ef569c4244
Joshua Montross <joshua.montross@gmail.com>
2012-08-12 16:32:18 -0700

    more gems for pdf makin

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem 'sinatra'
+gem 'pdfkit'
 gem 'wkhtmltopdf-heroku', '1.0.0'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'yaml'
+require 'wkhtmltopdf-heroku'
 require 'pdfkit'
 
 get '/' do
