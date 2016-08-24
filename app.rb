6d8e46ab2155b47adc8073b27be47f1d992978ac
Joshua Montross <joshua.montross@gmail.com>
2016-08-24 16:49:24 -0400

    removes stupid coder wall

--- app.rb
+++ app.rb
@@ -93,7 +93,7 @@ __END__
                       <a class='twitter' href='http://twitter.com/#!/<%= @awesome_instance_variables[:twitter] %>'></a>
                       <a class='linkedin' href='http://www.linkedin.com/in/<%= @awesome_instance_variables[:linkedin] %>'></a>
                       <a class='github' href='https://github.com/<%= @awesome_instance_variables[:github] %>'></a>
-                      <a href="https://coderwall.com/jmontross"><img alt="Endorse jmontross on Coderwall" src="https://api.coderwall.com/jmontross/endorsecount.png" /></a>
+                      <!-- <a href="https://coderwall.com/jmontross"><img alt="Endorse jmontross on Coderwall" src="https://api.coderwall.com/jmontross/endorsecount.png" /></a> -->
                       <br style='clear:both' />
                       <br style='clear:both' />
 
